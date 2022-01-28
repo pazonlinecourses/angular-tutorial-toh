d747eb69d5ee710af2bbc6442f0874c081a4e23a
PazJal <30651575+PazJal@users.noreply.github.com>
2022-01-28 21:34:18 +0200

    feat: heroes detail is now its own view
    move hero detail to seprate view. create route. connect links from other components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
 
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 import { MessageService } from '../message.service';
 
@@ -12,7 +12,6 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero? : Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
@@ -22,11 +21,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
