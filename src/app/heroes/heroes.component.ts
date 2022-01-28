2c0df8b11a819bd1c4fb9be9666de545f39b06a2
PazJal <30651575+PazJal@users.noreply.github.com>
2022-01-28 16:12:26 +0200

    feat: add messages component
    add message component, wire to message service.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +14,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero? : Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -25,6 +26,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
