c969599659948a4b438ad2fabdc4e5dc94d3553a
PazJal <30651575+PazJal@users.noreply.github.com>
2022-01-28 15:40:25 +0200

    refactor: move data fetch out of component into service
    create heroes service. inject hero service into component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 import { Hero } from '../hero';
 
@@ -10,15 +10,21 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero? : Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
 }
